51c0e11b54026124cd0467a140ca38e9ab069b85
Brooke Pauly <bpauly2003@gmail.com>
2026-07-18 21:11:35 -0400

    Change to commands

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord import app_commands
 
 load_dotenv()
 
 # checks bot is connected
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f' Logged on as {self.user}!')
 
 # allowing bot access to specific discord functions
 intents = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix= "!", intents = intents)
+
+
+@client.tree.command(name = "Export", description = "")
 
 # run the bot
-client = Client(intents=intents)
 client.run(os.environ.get("DISCORD_TOKEN"))
